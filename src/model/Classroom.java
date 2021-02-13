88526dfab33ebe9159bb73c206b1ebe6f8064340
nicolas gomez <nicolasg1911@gmail.com>
2021-02-12 21:03:23 -0500

    first comit

---
+++ src/model/Classroom.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Classroom {
+
+	public Classroom() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/ui/ClassroomGUI.java
@@ -0,0 +1,12 @@
+package ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
+public class ClassroomGUI {
+
+    @FXML
+    private Pane mainPane;
+
+}
+
