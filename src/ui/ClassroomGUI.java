81f54f4797945bc470ac5a7f762f0156fc787558
nicolas gomez <nicolasg1911@gmail.com>
2021-02-12 21:35:43 -0500

    second commit

--- src/ui/ClassroomGUI.java
+++ src/ui/ClassroomGUI.java
@@ -1,12 +1,60 @@
 package ui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import model.Classroom;
 
 public class ClassroomGUI {
+	private Classroom classroom;
+
+    @FXML 
+    private Pane mainPane;
+    
+    @FXML
+    private TextField txtUsername;
 
     @FXML
-    private Pane mainPane;
+    private TextField txtPassword;
+
+   
+    @FXML
+    private TextField txtUserNameRegister;
+
+    @FXML
+    private TextField txtPasswordRegister;
+    
+    public ClassroomGUI(Classroom classroom) {
+    	this.classroom=classroom;
+    }
+   
+	
+	
+
+    @FXML
+    public void createAccount(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void signIn(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void logIn(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void singUp(ActionEvent event) {
+    	
+
+    }
 
 }
 
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -5,20 +5,36 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import model.Classroom;
+import model.UserAccount;
 public class Main extends Application {
+	private ClassroomGUI classroomGUI;
+	private Classroom classroom;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		launch(args);
 	}
-
+public Main() {
+	classroomGUI=new ClassroomGUI(classroom)  ;
+	classroom=new Classroom()  ;
+	
+}
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("main-pane,fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("main-pane.fxml"));
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Classroom");
 		primaryStage.show();
+	}
+	public void change(Stage primaryStage) throws Exception{
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login.fxml"));
+		
+		fxmlLoader.setController(this);    	
+		Parent loginPane = fxmlLoader.load();
+		
+		/*classroomGUI.mainPane.getChildren().clear();
+		classroomGUI.mainPane.setAll(loginPane);*/
 	}
 }
